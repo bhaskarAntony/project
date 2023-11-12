de54a6ae02ced5b15e64cccb19b07e827b943f15
bhaskarAntony <rooparoopa7105@gmail.com>
2023-11-13 00:20:14 +0530

    updated files

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -3,27 +3,33 @@ import '../styles/hero.css'
 import BookForm from './RegistrationForm/BookForm'
 import ApplyNow from '../Helpers/ApplyNow'
 import { Carousel } from 'react-bootstrap';
+import ReadMore from '../Helpers/ReadMore';
 
 const home = [
   {
-    heading: "High Paid Salaries & Continuous Career Growth",
-    subheading: "Build skills with courses, certificates, and degrees online from world-class universities and companies.",
-    image: "https://images.pexels.com/photos/7606074/pexels-photo-7606074.jpeg?auto=compress&cs=tinysrgb&w=600",
+    heading: "Flexible Hourly Car Rentals for Your Convenience",
+    subheading: "Discover the freedom of flexible travel with our hourly car rental service. Perfect for short trips, errands, or city explorations, our fleet of reliable vehicles awaits your command. Enjoy the convenience of on-demand transportation, hassle-free bookings, and pay only for the time you need.",
+    tag: "Hourly Car Rental",
   },
   {
-    heading: "High Paid Salaries & Continuous Career Growth",
-    subheading: "Build skills with courses, certificates, and degrees online from world-class universities and companies.",
-    image: "https://images.pexels.com/photos/7606074/pexels-photo-7606074.jpeg?auto=compress&cs=tinysrgb&w=600",
+    heading: "Explore Beyond Boundaries with Our Outstation Car Rentals",
+    subheading: "Embark on unforgettable journeys with our outstation car rental services. Whether it's a weekend getaway or an extended road trip, our well-maintained fleet and experienced drivers ensure a comfortable and safe travel experience. Discover new horizons with the convenience of door-to-door service and transparent pricing",
+    tag: "Outstation Car Rental in Bangalore",
   },
   {
-    heading: "High Paid Salaries & Continuous Career Growth",
-    subheading: "Build skills with courses, certificates, and degrees online from world-class universities and companies.",
-    image: "https://images.pexels.com/photos/7606074/pexels-photo-7606074.jpeg?auto=compress&cs=tinysrgb&w=600",
+    heading: "Seamless Airport Transfers with Our Car Rental Services",
+    subheading: "Start and end your journey with ease using our airport car rental services. Enjoy stress-free airport transfers with our punctual and professional drivers. From the runway to your destination, our reliable fleet ensures a smooth and comfortable ride. Make your travel experience seamless with our convenient airport car rental options.",
+    tag: "Airport Car Rental in Bangalore",
   },
   {
-    heading: "High Paid Salaries & Continuous Career Growth",
-    subheading: "Build skills with courses, certificates, and degrees online from world-class universities and companies.",
-    image: "https://images.pexels.com/photos/7606074/pexels-photo-7606074.jpeg?auto=compress&cs=tinysrgb&w=600",
+    heading: "Elevate Your Travel Experience with Luxury Car Rentals",
+    subheading: "Indulge in the epitome of sophistication and style with our luxury car rental services. Experience the thrill of driving or being chauffeured in top-tier vehicles that redefine opulence. Whether for a special occasion or a taste of luxury, our premium fleet is designed to exceed your expectations. Elevate your journey with our luxury car rental options.",
+    tag: "Luxury Car Rental in Bangalore",
+  },
+  {
+    heading: "Efficient and Reliable Employee Transportation Solutions",
+    subheading: "Streamline your workforce's commute with our dedicated employee transportation services. We understand the importance of punctuality and reliability in corporate travel. Our fleet of well-maintained vehicles and experienced drivers ensure that your employees reach their destination comfortably and on time. Enhance productivity and employee satisfaction with our tailored transportation solutions",
+    tag: "Employee Transportation Services in Bangaore",
   },
 ];
 function HomeHero() {
@@ -75,9 +81,9 @@ function HomeHero() {
           {home.map((item, index) => (
             <Carousel.Item key={index}>
                   <div className="hero-text p-lg-5 p-md-3 p-3">
-                    <h1 className="card-heading fs-4 text-white text-start">Best Car Rentals in Bangalore</h1>
+                    <h1 className="card-heading fs-4 text-white text-start">{item.tag}</h1>
                     <h1 className="heading text-main text-900 text-start">{item.heading}</h1>
-                    <p className="text-white fs-5 my-4 text-start">{item.subheading}</p>
+                    <p className="text-white fs-5 my-4 text-start"><ReadMore text={item.subheading} maxLength={100}/></p>
                     <div className="carousel-footer w-100 d-flex flex-wrap">
                       <button className='main-btn bg-white text-black  m-2 hero-btn py-3'>Explore All Cars<i class="bi bi-chevron-double-right"></i></button>
                       <button className='main-btn m-2 hero-btn py-3' onClick={openModal}>Check Avail Cars <i class="bi bi-chevron-double-right"></i></button>
